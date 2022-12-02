2d0cf68de1ec9804a247d7649c0777ce513543b4
Arnab Biswas <khanp4397@gmail.com>
2022-12-02 15:46:38 +0530

    Added Header and Banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,10 +14,10 @@ function Header() {
         <div className="mt-2 flex items-center flex-grow-0 md:flex-grow ">
           <Image
             src="https://links.papareact.com/f90"
-            width={150}
+            width={70}
             height={40}
             objectFit="contain"
-            className="cursor-pointer"
+            className="cursor-pointer mx-2"
           />
         </div>
 
@@ -40,8 +40,13 @@ function Header() {
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="link">
+          <div className="link relative flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              4
+            </span>
+
             <ShoppingCartIcon className="h-10" />
+
             <p className="font-extrabold md:text-sm hidden md:inline mt-2">
               Basket
             </p>
@@ -50,6 +55,21 @@ function Header() {
       </div>
 
       {/* Bottom Nav */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </nav>
   );
 }
---
+++ src/components/ProductFeed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+async function ProductFeed() {
+  return (
+    <div>
+      <h1>Product Feed</h1>
+    </div>
+  );
+}
+
+export default ProductFeed;
